61d142186c5a91f44c0da524e72fbd192700e703
BritanyLM <britanylm@gmail.com>
2024-10-30 18:13:05 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+import React from 'react';
+import './App.css'
+import ContactList from './components/contactlist';
+
+export default function App() {
+  return (
+    <>
+      <ContactList />
+    </>
+  );
+}
